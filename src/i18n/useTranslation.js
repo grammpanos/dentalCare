4e144fdf1f200e4b13df40071a013f1a23c1669e
grammpanos <93728754+grammpanos@users.noreply.github.com>
2026-05-26 17:38:10 +0300

    Delete src directory

--- src/i18n/useTranslation.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { LanguageContext } from "../context/LanguageContext";
-import { translations } from "./translations";
-
-export function useTranslation() {
-  const { locale } = useContext(LanguageContext);
-  return translations[locale] || translations.gr;
-}
